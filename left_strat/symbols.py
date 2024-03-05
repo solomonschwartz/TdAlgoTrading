2e0572e5302786f4011fdb1d94c8e62634fcd3f8
solomonschwartz <sschwar8@mail.yu.edu>
2024-03-05 10:29:15 -0500

    TdAlgoTrading - added writeup

--- left_strat/symbols.py
+++ left_strat/symbols.py
@@ -1,13 +1,5 @@
-# symbols = ["TQQQ", "SOXL", "QLD", "SSO", "SPXL", "UPRO", "FNGO", "TECL",
-#            "TNA", "FAS", "NRGU", "LABU", "TSLL", "BULZ", "DPST", "UDOW"]
 
 
 # note that SOXL seems to perform poorly, XXXX seems to do ok
 leverage = {"TQQQ": 3, "SOXL": 3, "QLD": 2, "SSO": 2, "SPXL": 3, "UPRO": 3, "FNGO": 3, "TECL": 3, "TNA": 3, "FAS": 3,
             "NRGU": 3, "LABU": 3, "TSLL": 1.5, "BULZ": 3, "DPST": 3, "UDOW": 3}
-
-# symbols = ["LABU", "ERX", "UYG"]
-
-# symbols = ["UYG", "ROM", "NUGT", "GUSH", "DDM", "XDEC", "USD", "URTY"]
-
-# done through sept: LABU, TQQQ, SOXL,"QLD", "SSO", "SPXL", UPRO, FNGO, TECL, TNA, FAS, NRGU, TSLL,
